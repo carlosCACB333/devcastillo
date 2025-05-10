ea16579e4fb0b6fd53f26daa554a999ef2b25e33
Carlos <carloscacb333@gmail.com>
2025-05-10 12:14:29 -0500

    feat: update personal biography and skills in aboutme.ts

--- src/assets/aboutme.ts
+++ src/assets/aboutme.ts
@@ -1,153 +1,165 @@
-export const aboutme = `Biografía: Bachiller en Ingeniería de Sistemas e Informática en la Universidad Nacional Santiago Antúnez de Mayolo (UNASAM). Con experiencia en desarrollo de aplicaciones de servidor, web, móviles, escritorio y ciencia de datos que impulsan el desarrollo en el mundo digital. Me considero una persona proactiva, autodidacta y con capacidad para trabajar en equipo. Me apasiona la tecnología y el desarrollo de software, por lo que siempre
-estoy en constante aprendizaje y actualización.
-.
+export const aboutme = `
 
-En mi tiempo libre, paseo a mi perro Toby y disfruto de ver películas y series de comedia. Escucho reggaetón y música electrónica. Mi color favorito es el negro y disfruto la comida peruana, especialmente el ceviche, arroz con mariscos y frituras.
+# Biografía:
+    Bachiller en Ingeniería de Sistemas e Informática en la Universidad Nacional Santiago Antúnez de Mayolo (UNASAM). Con experiencia en desarrollo de aplicaciones de servidor, web, móviles, escritorio y ciencia de datos que impulsan el desarrollo en el mundo digital. Me considero una persona proactiva, autodidacta y con capacidad para trabajar en equipo. Me apasiona la tecnología y el desarrollo de software, por lo que siempre estoy en constante aprendizaje y actualización.
 
-Tecnologías y Herramientas:
-Lenguajes: Python, TypeScript, Golang, Node.js, Java, SQL, rust.
-Backend: Express.js, NestJS, Gin, Fiber, FastAPI, Django, Django Rest Framework,fastApi, actix web,.
-Frontend: ReactJS, Next.js.
-Librerías de UI: Material UI, Chakra UI, Next UI, TailwindCSS, Bootstrap.
-IDE favorito: VSCode.
-Versionamiento: Git, GitHub, AWS code commit.
-Metodologías ágiles: Scrum.
-Contenedores: Docker, Docker-compose, kubernetes..
-Bases de datos: PostgreSQL, MySQL, MongoDB, SQL Server
-Bases de datos nosql: MongoDB,Dynamodb.
-DevOps: Kubernetes, GitHub Actions.
-Ciencia de datos: Python (Pandas, NumPy, Matplotlib, Seaborn, Plotly, Scikit-learn, TensorFlow, Keras).
-Datos personales:
-Desarrollo móvil: React Native (con Expo).
-Desarrollo de escritorio: Java y Python (Tkinter, PyQt5).
-Cloud computing: AWS (EC2, S3, RDS, Lambda, API Gateway, CloudFront, CloudWatch, CloudFormation, Route 53, Elastic Beanstalk).
+    En mi tiempo libre, paseo a mi perro Toby y disfruto de ver películas y series de comedia. Escucho reggaetón y música electrónica. Mi color favorito es el negro y disfruto la comida peruana, especialmente el ceviche, arroz con mariscos y frituras.
 
-Nombre: Carlos Antonio Castillo Blas
-Edad: 25 años
-Ciudad: Huaraz, Ancash, Perú
-Estado civil: Soltero
-Estatura: 1.73 m
-Peso: 68 kg
-Correo: carloscb8080@gmail.com
-LinkedIn: CarlosCastilloBlas
-GitHub: CarlosCACB333
-Página web: carloscb.com
+# Tecnologías y Herramientas:
+    - Lenguajes: Python, TypeScript, Golang, Node.js, Java, SQL, rust.
+    - Backend: Express.js, NestJS, Gin, Fiber, FastAPI, Django, Django Rest Framework,fastApi, actix web,.
+    - Frontend: ReactJS, Next.js.
+    - Librerías de UI: Material UI, Chakra UI, Next UI, TailwindCSS, Bootstrap.
+    - IDE favorito: VSCode.
+    - Versionamiento: Git, GitHub, AWS code commit.
+    - Metodologías ágiles: Scrum.
+    - Contenedores: Docker, Docker-compose, kubernetes..
+    - Bases de datos: PostgreSQL, MySQL, MongoDB, SQL Server
+    - Bases de datos nosql: MongoDB,Dynamodb.
+    - DevOps: Kubernetes, GitHub Actions.
+    - Ciencia de datos: Python (Pandas, NumPy, Matplotlib, Seaborn, Plotly, Scikit-learn, TensorFlow, Keras).
+    - Datos personales:
+    - Desarrollo móvil: React Native (con Expo).
+    - Desarrollo de escritorio: Java y Python (Tkinter, PyQt5).
+    - Cloud computing: AWS (EC2, S3, RDS, Lambda, API Gateway, CloudFront, CloudWatch, CloudFormation, Route 53, Elastic Beanstalk).
+
+# Datos personales:
+    - Nombre: Carlos Antonio Castillo Blas
+    - Edad: 26 años
+    - Ciudad: Lima, Perú
+    - Estado civil: Soltero
+    - Estatura: 1.73 m
+    - Peso: 68 kg
+    - Correo: carloscb8080@gmail.com
+    - LinkedIn: CarlosCastilloBlas
+    - GitHub: CarlosCACB333
+    - Página web: devcastillo.com
 
 
-Habilidades blandas:Trabajo colaborativo,Perseverancia,Autodidacta,Comunicación,Liderazgo,Adaptación al cambio,Dedicación, Responsabilidad.
+# Habilidades blandas:
+    Trabajo colaborativo,Perseverancia,Autodidacta,Comunicación,Liderazgo,Adaptación al cambio,Dedicación, Responsabilidad.
 
-Logros:Becario del PRONABEC,Estudiante del quinto superior en la UNASAM,Nivel “Legend” en PLATZI.
-Idiomas:Español (nativo),Inglés (básico).
+# Logros:
+    Becario del PRONABEC,Estudiante del quinto superior en la UNASAM,Nivel “Legend” en PLATZI.
 
-Educación:Ingeniería de Sistemas e Informática en UNASAM (2018-2023),Programa Oracle Next Education (ONE) especialización en Programación Backend (2023),Cursos autodidactas en Udemy y Platzi (2020-actualidad).
+# Educación:
+    Ingeniería de Sistemas e Informática en UNASAM (2018-2023),Programa Oracle Next Education (ONE) especialización en Programación Backend (2023),Cursos autodidactas en Udemy y Platzi (2020-actualidad).
 
-Experiencia laboral: Desarrollador Backend Jr en Netdreams (febrero 2023 - actualidad) usando python,fastapi,aws,dynamodb,lambda,dockers,ec2,fargate,mysql,postgresql,git,codecommit,api rest.
-Proyectos personales:
+# Experiencia laboral: 
 
-Sistema web para JBCSYSTEM (2021): React, Django Rest Framework, PostgreSQL.
-App móvil para manejo de deudas y préstamos (2022): React Native, Node.js, TypeScript, MongoDB.
-Sitio web personal (2023): Next.js, NextUI, Golang, Fiber, PostgreSQL, Hygraph.
+1.Desarrollador FrontEnd en TypeSubs (Octubre 2024 - Actualidad)
+    -Desarrollé interfaces modernas, accesibles, intuitivas, escalables y mantenibles.
+    -Implementé pagos mediante Mercado Pago e integré sistemas de autenticación seguros con Auth0.
+    -Optimizé la experiencia del usuario a través de streaming de datos, reduciendo los tiempos de carga.
+    -Apliqué diferentes estrategias de renderizado (SSR, SSG, ISR) para mejorar el rendimiento y SEO.
+    -Trabajé con Node.js, React.js, Next.js, Tailwind CSS, Redux y TypeScript.
+2.Desarrollador Backend  en Netdreams (Enero 2021 - Diciembre 2024)
+    -Desarrollé e implementé microservicios escalables para la billetera digital BIM, asegurando alta disponibilidad y mantenibilidad.
+    -Optimizé el rendimiento de consultas a bases de datos y endpoints de APIs, mejorando tiempos de respuesta y reduciendo la carga del sistema.
+    -Diseñé e implementé soluciones de interoperabilidad con otras billeteras digitales, facilitando integraciones seguras y ecientes.
+    -Desarrollé el nuevo core backend capaz de manejar peticiones concurrentes a gran escala, incluyendo una refactorización profunda del código legado.
+    -Utilicé tecnologías modernas como Python, FastAPI, PostgreSQL, MySQL, DynamoDB, AWS y Docker para construir soluciones robustas y orientadas a rendimiento.
 
-Certificaciones y cursos destacados:
-React Native: Aplicaciones nativas para IOS y Android
-Next.js: El framework de React para producción
-Backend con Node.js: API REST con Express.js
-Manipulación y Transformación de Datos con Pandas y NumPy
-Visualización de Datos con Matplotlib y Seaborn
-Análisis de Negocios para Ciencia de Datos
-Análisis Exploratorio de Datos
-Aplicaciones en Tiempo Real con Socket.io
-Asincronismo con JavaScript
-Backend con Node.js: API REST con Express.js
-Backend con Node.js: Autenticación con Passport.js y JWT
-Backend con Node.js: Base de Datos con PostgreSQL
-Configuración Profesional de Entorno de Trabajo para Ciencia de Datos
-CSS Grid Básico
-Django Intermedio: Testing, Static Files, Django Admin
-Economía Digital: Bitcoin y Criptomonedas
-Entorno de Trabajo para Ciencia de Datos con Jupyter Notebooks y Anaconda
-Ethereum para Developers
-Frontend Developer
-Funciones Matemáticas para Data Science e Inteligencia Artificial
-Fundamentos de Álgebra Lineal con Python
-Fundamentos de Bases de Datos
-Fundamentos de Redes Neuronales con Python y Keras
-Fundamentos de TypeScript
-Fundamentos Prácticos de Machine Learning
-Inglés Básico: Escritura
-Introducción a Java SE
-Introducción a la Terminal y Línea de Comandos
-Introducción al Desarrollo Backend
-Introducción al Desarrollo Blockchain: Smart Contracts
-Introducción a Librerías UI con React
-Introducción al Pensamiento Computacional con Python
-Introducción a Machine Learning por MindsDB
-Introducción a React.js
-Introducción a Solidity
-Introducción a Tokens en Ethereum
-Java SE Orientado a Objetos
-Java SE Persistencia de Datos
-Manipulación y Análisis de Datos con Pandas y Python
-Matemáticas para Data Science: Estadística Descriptiva
-Matemáticas para Data Science: Probabilidad
-Next.js
-Next.js con GraphQL
-Next.js: Internacionalización de Aplicaciones Web con i18n
-Next.js: Optimización y Deploy a Producción
-Next.js: Optimización y Manejo de Grandes Datasets
-Next.js: Sitios Estáticos y Jamstack
-Node.js: Base de Datos con MongoDB y WebSockets
-Node.js: GraphQL con Apollo Server y Prisma
-PostgreSQL
-PostgreSQL Aplicado a Ciencia de Datos
-Prework: Configuración de Entorno de Desarrollo en Windows
-React Avanzado
-React.js: Manejo Profesional del Estado
-React.js: Patrones de Render y Composición
-React Native CLI
-React Router 5 y Redux
-Regresión Lineal con Python y scikit-learn
-State Machines en React.js
-TensorFlow.js
-TypeScript: Programación Orientada a Objetos y Asincronismo
-TypeScript: Tipos Avanzados y Funciones
-UX para DeFi: Diseñando los Servicios Financieros del Futuro
-Vite.js
-curso Práctico de React.js
-curso Profesional de Machine Learning con Scikit-Learn
-curso Profesional de Next.js
-curso Profesional de React Hooks
-curso Profesional de React.js y Redux
-curso Profesional de Scrum
-curso Práctico de Go: Creación de un Servidor Web
-Full Stack Developer con JavaScript
-Frontend con React.js
-React PRO: Lleva tus bases al siguiente nivel
-Nest: Desarrollo backend escalable con Node
-Go Intermedio: Programación Orientada a Objetos y Concurrencia
-Introducción a AWS: Fundamentos de Cloud Computing
-Expresiones Regulares
-Python: Comprehensions, Funciones y Manejo de Errores
-Python: PIP y Entornos Virtuales
-Introducción a AWS: Cómputo, Almacenamiento y Bases de Datos
-curso Práctico de AWS: Roles y Seguridad con IAM
-curso Práctico de AWS: Cómputo con EC2
-Fundamentos de Ingeniería de Software
-curso Práctico de AWS Cloud
-curso Práctico de Cloud Computing con AWS
-curso Práctico de Bases de Datos en AWS
-curso Práctico de Storage en AWS
-Networking y Content Delivery en AWS
-Amazon Web Services
-Introducción a Serverless, Lambdas y Api Gateway con AWS
-AWS Lambda y API Gateway
-Docker - Guía práctica de uso para desarrolladores
-desarrollo backend - oracle next education
-Inteligencia Artificial y Deep Learning desde cero en Python
-Go Avanzado: Concurrencia y Patrones de Diseño
-Go Avanzado: REST y WebSockets
-Unit Testing en Go
-Go Avanzado: Protobuffers y gRPC
-Go Avanzado: Arquitectura de Eventos y CQRS
-Ruta de Aprendizaje de Backend con Go(golang)
-`;
+# Certificaciones y cursos destacados:
+    -React Native: Aplicaciones nativas para IOS y Android
+    -Next.js: El framework de React para producción
+    -Backend con Node.js: API REST con Express.js
+    -Manipulación y Transformación de Datos con Pandas y NumPy
+    -Visualización de Datos con Matplotlib y Seaborn
+    -Análisis de Negocios para Ciencia de Datos
+    -Análisis Exploratorio de Datos
+    -Aplicaciones en Tiempo Real con Socket.io
+    -Asincronismo con JavaScript
+    -Backend con Node.js: API REST con Express.js
+    -Backend con Node.js: Autenticación con Passport.js y JWT
+    -Backend con Node.js: Base de Datos con PostgreSQL
+    -Configuración Profesional de Entorno de Trabajo para Ciencia de Datos
+    -CSS Grid Básico
+    -Django Intermedio: Testing, Static Files, Django Admin
+    -Economía Digital: Bitcoin y Criptomonedas
+    -Entorno de Trabajo para Ciencia de Datos con Jupyter Notebooks y Anaconda
+    -Ethereum para Developers
+    -Frontend Developer
+    -Funciones Matemáticas para Data Science e Inteligencia Artificial
+    -Fundamentos de Álgebra Lineal con Python
+    -Fundamentos de Bases de Datos
+    -Fundamentos de Redes Neuronales con Python y Keras
+    -Fundamentos de TypeScript
+    -Fundamentos Prácticos de Machine Learning
+    -Inglés Básico: Escritura
+    -Introducción a Java SE
+    -Introducción a la Terminal y Línea de Comandos
+    -Introducción al Desarrollo Backend
+    -Introducción al Desarrollo Blockchain: Smart Contracts
+    -Introducción a Librerías UI con React
+    -Introducción al Pensamiento Computacional con Python
+    -Introducción a Machine Learning por MindsDB
+    -Introducción a React.js
+    -Introducción a Solidity
+    -Introducción a Tokens en Ethereum
+    -Java SE Orientado a Objetos
+    -Java SE Persistencia de Datos
+    -Manipulación y Análisis de Datos con Pandas y Python
+    -Matemáticas para Data Science: Estadística Descriptiva
+    -Matemáticas para Data Science: Probabilidad
+    -Next.js
+    -Next.js con GraphQL
+    -Next.js: Internacionalización de Aplicaciones Web con i18n
+    -Next.js: Optimización y Deploy a Producción
+    -Next.js: Optimización y Manejo de Grandes Datasets
+    -Next.js: Sitios Estáticos y Jamstack
+    -Node.js: Base de Datos con MongoDB y WebSockets
+    -Node.js: GraphQL con Apollo Server y Prisma
+    -PostgreSQL
+    -PostgreSQL Aplicado a Ciencia de Datos
+    -Prework: Configuración de Entorno de Desarrollo en Windows
+    -React Avanzado
+    -React.js: Manejo Profesional del Estado
+    -React.js: Patrones de Render y Composición
+    -React Native CLI
+    -React Router 5 y Redux
+    -Regresión Lineal con Python y scikit-learn
+    -State Machines en React.js
+    -TensorFlow.js
+    -TypeScript: Programación Orientada a Objetos y Asincronismo
+    -TypeScript: Tipos Avanzados y Funciones
+    -UX para DeFi: Diseñando los Servicios Financieros del Futuro
+    -Vite.js
+    -curso Práctico de React.js
+    -curso Profesional de Machine Learning con Scikit-Learn
+    -curso Profesional de Next.js
+    -curso Profesional de React Hooks
+    -curso Profesional de React.js y Redux
+    -curso Profesional de Scrum
+    -curso Práctico de Go: Creación de un Servidor Web
+    -Full Stack Developer con JavaScript
+    -Frontend con React.js
+    -React PRO: Lleva tus bases al siguiente nivel
+    -Nest: Desarrollo backend escalable con Node
+    -Go Intermedio: Programación Orientada a Objetos y Concurrencia
+    -Introducción a AWS: Fundamentos de Cloud Computing
+    -Expresiones Regulares
+    -Python: Comprehensions, Funciones y Manejo de Errores
+    -Python: PIP y Entornos Virtuales
+    -Introducción a AWS: Cómputo, Almacenamiento y Bases de Datos
+    -curso Práctico de AWS: Roles y Seguridad con IAM
+    -curso Práctico de AWS: Cómputo con EC2
+    -Fundamentos de Ingeniería de Software
+    -curso Práctico de AWS Cloud
+    -curso Práctico de Cloud Computing con AWS
+    -curso Práctico de Bases de Datos en AWS
+    -curso Práctico de Storage en AWS
+    -Networking y Content Delivery en AWS
+    -Amazon Web Services
+    -Introducción a Serverless, Lambdas y Api Gateway con AWS
+    -AWS Lambda y API Gateway
+    -Docker - Guía práctica de uso para desarrolladores
+    -desarrollo backend - oracle next education
+    -Inteligencia Artificial y Deep Learning desde cero en Python
+    -Go Avanzado: Concurrencia y Patrones de Diseño
+    -Go Avanzado: REST y WebSockets
+    -Unit Testing en Go
+    -Go Avanzado: Protobuffers y gRPC
+    -Go Avanzado: Arquitectura de Eventos y CQRS
+    -Ruta de Aprendizaje de Backend con Go(golang)
+    `;
