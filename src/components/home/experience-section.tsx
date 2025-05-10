73e2a282b0b17062a555c272814ea0b3d7510801
Carlos <carloscacb333@gmail.com>
2025-05-10 11:23:08 -0500

    feat: enhance experience section with detailed job descriptions and new components

--- src/components/home/experience-section.tsx
+++ src/components/home/experience-section.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { sectionWrapper, subtitle, title } from "@/components";
+import { ReactNode } from "react";
 import { MdCheck, MdWorkHistory } from "react-icons/md";
 
 export const ExperienceSection = () => {
@@ -33,39 +34,103 @@ export const ExperienceSection = () => {
         <br />
         <br />
 
-        <div className="flex flex-col items-center">
-          <ol className="relative border-s border-gray-200 dark:border-gray-700 max-w-md">
-            <li className=" ms-8">
-              <span className="absolute flex items-center justify-center  rounded-full -start-5 ring-8  ring-pink-200 dark:ring-pink-950 bg-pink-500 p-2">
-                <MdCheck className="text-background" size={20} />
-              </span>
+        <Wizard>
+          <WizardItem>
+            <h3 className={title({ size: "xs" })}>
+              <span className="text-pink-500">Desarrollador FrontEnd</span>
+              &nbsp;en TypeSubs
+            </h3>
 
-              <h3 className={title({ size: "xs" })}>
-                <span className="text-pink-500">Desarrollador Backend </span>
-                &nbsp;en Netdreams
-              </h3>
+            <time className="block mb-2 text-base italic leading-none opacity-80">
+              Octubre 2024 - Actualidad
+            </time>
 
-              <time className="block mb-2 text-base italic leading-none opacity-80">
-                Enero 2021 - Actualidad
-              </time>
-              <p className={subtitle({ fullWidth: true })}>
-                Implementé y mantuve los microservicios de la billetera
-                digital&nbsp;
-                <span className="text-pink-500">BIM</span> usando tecnologías
-                como:
-              </p>
+            <ul className="list-disc list-outside marker:text-pink-500 ms-4">
+              <li>
+                Desarrollé interfaces modernas, accesibles, intuitivas,
+                escalables y mantenibles.
+              </li>
+              <li>
+                Implementé pagos mediante Mercado Pago e integré sistemas de
+                autenticación seguros con Auth0.
+              </li>
+              <li>
+                Optimizé la experiencia del usuario a través de streaming de
+                datos, reduciendo los tiempos de carga.
+              </li>
+              <li>
+                Apliqué diferentes estrategias de renderizado (SSR, SSG, ISR)
+                para mejorar el rendimiento y SEO.
+              </li>
+              <li>
+                Trabajé con Node.js, React.js, Next.js, Tailwind CSS, Redux y
+                TypeScript.
+              </li>
+            </ul>
+          </WizardItem>
+          <WizardItem>
+            <h3 className={title({ size: "xs" })}>
+              <span className="text-pink-500">Desarrollador Backend </span>
+              &nbsp;en Netdreams
+            </h3>
 
-              <ul className="list-disc list-inside marker:text-pink-500 pl-4  ">
-                <li>Python - FastAPI</li>
-                <li>AWS - Lambda, S3, SQS, SNS, RDS, DynamoDB</li>
-                <li>Docker - Docker Compose - Kubernetes</li>
-                <li> PostgreSQL - MySQL</li>
-                <li>Git - CodeCommit</li>
-              </ul>
-            </li>
-          </ol>
-        </div>
+            <time className="block mb-2 text-base italic leading-none opacity-80">
+              Enero 2021 - Diciembre 2024
+            </time>
+
+            <ul className="list-disc list-outside marker:text-pink-500 ms-4">
+              <li>
+                Desarrollé e implementé microservicios escalables para la
+                billetera digital BIM, asegurando alta
+              </li>
+              <li>disponibilidad y mantenibilidad.</li>
+              <li>
+                Optimizé el rendimiento de consultas a bases de datos y
+                endpoints de APIs, mejorando tiempos de
+              </li>
+              <li>respuesta y reduciendo la carga del sistema.</li>
+              <li>
+                Diseñé e implementé soluciones de interoperabilidad con otras
+                billeteras digitales, facilitando
+              </li>
+              <li>
+                integraciones seguras y ecientes.Desarrollé el nuevo core
+                backend capaz de manejar peticiones
+              </li>
+              <li>
+                concurrentes a gran escala, incluyendo una refactorización
+                profunda del código legado.
+              </li>
+              <li>
+                Utilicé tecnologías modernas como Python, FastAPI, PostgreSQL,
+                MySQL, DynamoDB, AWS y Docker
+              </li>
+              <li>
+                para construir soluciones robustas y orientadas a rendimiento.
+              </li>
+            </ul>
+          </WizardItem>
+        </Wizard>
       </section>
     </>
+  );
+};
+
+const Wizard = ({ children }: { children: ReactNode }) => {
+  return (
+    <ol className="relative border-s border-gray-200 dark:border-gray-700 max-w-2xl flex flex-col gap-4 list-inside">
+      {children}
+    </ol>
+  );
+};
+
+const WizardItem = ({ children }: { children: ReactNode }) => {
+  return (
+    <li className="ms-8">
+      <span className="absolute flex items-center justify-center  rounded-full -start-5 ring-8  ring-pink-200 dark:ring-pink-950 bg-pink-500 p-2">
+        <MdCheck className="text-background" size={20} />
+      </span>
+      {children}
+    </li>
   );
 };
